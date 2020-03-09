6867bfd12ee8643f0431eed5568e6025c3a3149d
m5khan <mshoaibkhan91@gmail.com>
2020-03-09 21:01:32 +0100

    configure webpack and setup react

--- src/providers/web.ts
+++ src/providers/web.ts
@@ -28,7 +28,7 @@ export class WebProvider implements Provider {
         server.use(helmet());
         server.use(cors());
         server.use(express.json());
-
+        server.use(express.static("public"));
         /** Adding routers */
         server.use("/tweets", tweetRouter);
 
--- webpack.config.ts
+++ webpack.config.ts
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 
-module.exports = {
+const serverConfig = {
     entry: ["./src/index.ts"],
     devtool: 'inline-source-map',
     //watch: true,
@@ -28,6 +28,56 @@ module.exports = {
         filename: "server.js"
     }
 }
+
+const webConfig = {
+    mode: "development",
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "inline-source-map",
+    entry: ["./app/index.tsx"],
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+    target: 'web', // <=== can be omitted as default is 'web'
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'client.js'
+    },
+    
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                //exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+    
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    // externals: {
+    //     "react": "React",
+    //     "react-dom": "ReactDOM"
+    // }
+}
+
+module.exports = [
+    serverConfig, 
+    webConfig
+];
 
 // implement hot reload if application goes big
 // https://docs.nestjs.com/recipes/hot-reload
